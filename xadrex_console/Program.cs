8a08fea88d7b78902080d6d24ad841b1e4c7e915
Flávio Pinheiro <fpssouzza@gmail.com>
2019-08-03 23:16:03 -0300

    Inicio do projeto

---
+++ xadrex_console/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace xadrex_console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
